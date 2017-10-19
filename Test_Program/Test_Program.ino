a9a72349ec43f9756f7934db76bcac3100432761
Julian Jaramillo <jjaram48@gmail.com>
2017-10-19 18:08:50 +0200

    first commit on test programm

--- Test_Program/Test_Program.ino
+++ Test_Program/Test_Program.ino
@@ -1,25 +1,3 @@
-/*
-  Blink
-  Turns on an LED on for one second, then off for one second, repeatedly.
-
-  Most Arduinos have an on-board LED you can control. On the UNO, MEGA and ZERO 
-  it is attached to digital pin 13, on MKR1000 on pin 6. LED_BUILTIN is set to
-  the correct LED pin independent of which board is used.
-  If you want to know what pin the on-board LED is connected to on your Arduino model, check
-  the Technical Specs of your board  at https://www.arduino.cc/en/Main/Products
-  
-  This example code is in the public domain.
-
-  modified 8 May 2014
-  by Scott Fitzgerald
-  
-  modified 2 Sep 2016
-  by Arturo Guadalupi
-  
-  modified 8 Sep 2016
-  by Colby Newman
-*/
-
 int pin = 9;
 int t = 300; 
 
@@ -43,7 +21,7 @@ void setup() {
 
   digitalWrite(2, HIGH);    //Power Relay init
 
-  for (int thisPin = 7; thisPin < 13; thisPin++) { //Sets all pins HIGH before the program starts
+  for (int thisPin = 4; thisPin < 13; thisPin++) { //Sets all pins HIGH before the program starts
     digitalWrite(thisPin, HIGH);
   }
 }
@@ -51,69 +29,32 @@ void setup() {
 // the loop function runs over and over again forever
 void loop() {
 
-  digitalWrite(2, HIGH);    //Power Relay enable
+  digitalWrite(2, HIGH);    //Power Supply Relay enable
   
-  pin=random(4,100);          // the range adds 'silences' working range is 5 to 12 (for relays)
-  t = random(400,600);
+  pin = 7;          // the range adds 'silences' working range is 5 to 12 (for relays)
+  t = 100;
   
   Serial.println(pin);
   Serial.println(t);
   
-  digitalWrite(pin, LOW);   // turn the LED on (HIGH is the voltage level)
-  delay(t);                       // wait for a second
+  digitalWrite(pin, LOW);   // turn the Relay on by setting the level LOW
 
     if (pin == 7) {
     Serial.println("7! ON");
   }
 
-    if (pin == 8) {
-    Serial.println("8! ON");
-  }
-
-    if (pin == 9) {
-    Serial.println("9! ON");
-  }
-
-    if (pin == 10) {
-    Serial.println("10! ON");
-  }
-
-    if (pin == 11) {
-    Serial.println("11! ON");
-  }
-
-    if (pin == 12) {
-    Serial.println("12! ON");
-  }
+  delay(t);                       // wait for the time it will be turned on (while it is activated)
   
-  digitalWrite(pin, HIGH);    // turn the LED off by making the voltage LOW
-  delay(t);
+  digitalWrite(pin, HIGH);    // turn the Relay off by making the voltage HIGH again
+
 
   if (pin == 7) {
     Serial.println("7! OFF");
   }
 
-    if (pin == 8) {
-    Serial.println("8! OFF");
-  }
+//  delay(t);
 
-    if (pin == 9) {
-    Serial.println("9! OFF");
-  }
-
-    if (pin == 10) {
-    Serial.println("10! OFF");
-  }
-
-    if (pin == 11) {
-    Serial.println("11! OFF");
-  }
-
-    if (pin == 12) {
-    Serial.println("12! OFF");
-  }
-
-  digitalWrite(2, LOW);    //Power Relay disabled
+  digitalWrite(2, LOW);    //Power Supply Relay disabled
   
   delay(500); 
 }
